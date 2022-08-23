1cc50a3f3030b56870a280ef2e732c1f117e0bf8
Ruhul Juwel <ruhuljuwel111@gmail.com>
2022-08-23 19:39:43 +0600

    install react tailwind

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 function App() {
   return (
     <div>
-      <h2>Hello world</h2>
+      <h2 className='text-5xl'>Hello world</h2>
       <h3>This is clothes store</h3>
     </div>
   );
